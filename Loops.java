add16ff5fb599d0af21f413dae8f32ca40b8f9d2
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2022-01-10 00:16:37 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,28 @@
+package First;
+//print 1 to 10 in all 3 loops
+public class Loops {
+	public static void main(String args[])
+	{
+		int i=1;
+		//while loop
+		/*while(i<=10)
+		{
+			System.out.print(i);
+			i++;
+		}
+		
+		//do while loop*/
+		do
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+		
+		//for loop
+		for(int k=5;k<=10;k++)
+		{
+			System.out.println(k);
+		}
+	}
+
+}

bc4cf61ed815369dfc0cd564666589da75a5a425
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2022-01-09 23:14:41 +0530

    Create OpearatorsAll.java

---
+++ OpearatorsAll.java
@@ -0,0 +1,29 @@
+package First;
+
+public class OperatorsAll {
+	public static void main(String args[])
+	{
+		int a = 5 , b = 6;
+		//Arithmatic Operators
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		//Bitwise Opearator
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a<<2);
+		System.out.println(b>>2);
+		
+		//Ternary (condition)?True:False
+		System.out.println((a>b)?a:b);
+		
+		/*Condition operator
+		 * rational operator
+		 * assignment operator
+		 */
+	}
+
+}

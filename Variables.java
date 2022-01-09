39d22827c95450335c4b885452ad6cbfad04f5d8
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2022-01-09 23:14:18 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,25 @@
+package First;
+//Primitive Variables
+public class Variables {
+	public static void main(String args[])
+	{
+	// integer datatype
+	int a = 5;
+	int b = 6;
+	System.out.println(a+b);
+	
+    //float datatype
+	float c = 5.0f;
+	float d = 6.0f;
+	System.out.println(c+d);
+	
+	//character datatype
+	char e = 'A';
+	System.out.println(e);
+	
+	//Type Casting
+	System.out.println((int)e);
+	System.out.println((char)a);
+	System.out.println((char)b);
+}
+}
